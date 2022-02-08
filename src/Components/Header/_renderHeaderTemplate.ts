1f93c64943b398a9dd2ffd8584c3ebc7c168296f
Muhammed Abdrahman <muhammed03amin.com>
2022-02-08 14:48:37 +0600

    fix: fix module errors

--- src/Components/Header/_renderHeaderTemplate.ts
+++ src/Components/Header/_renderHeaderTemplate.ts
@@ -1,6 +1,6 @@
 import './headerStyle.scss';
 
-import styles from './styles.module.scss';
+import styles from './style.module.scss';
 
 export default function renderHeaderTemplate(): string {
   return `
